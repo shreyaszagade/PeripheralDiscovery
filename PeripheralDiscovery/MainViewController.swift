f1db5ba43e8c1fabf3c85bbcdf9115b52c9b8ef7
Shreyas Zagade <shreyaszagade@gmail.com>
2018-05-01 10:42:40 -0400

    Displayed bluetooth devices in close proximity and allowed to connect to them

--- PeripheralDiscovery/MainViewController.swift
+++ PeripheralDiscovery/MainViewController.swift
@@ -13,10 +13,17 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     var centralManager: CBCentralManager!
+    var peripheral = [String:CBPeripheral]()
+    var currentState : CBManagerState = .poweredOff
+    var selectedPeripheral : CBPeripheral?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         centralManager = CBCentralManager(delegate: self, queue: nil)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableViewAutomaticDimension;
+        tableView.estimatedRowHeight = 44.0;
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,11 +31,18 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func refreshButtonClicked(_ sender: UIBarButtonItem) {
+        
+        if currentState != .poweredOn {
+            print("Cannot access Bluetooth")
+        }else{}
+    }
+    
 }
 
 extension MainViewController : CBCentralManagerDelegate{
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        currentState = central.state
         switch central.state {
         case .poweredOn:
             centralManager.scanForPeripherals(withServices: nil)
@@ -40,5 +54,37 @@ extension MainViewController : CBCentralManagerDelegate{
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         self.peripheral[peripheral.identifier.uuidString] = peripheral
+        self.tableView.reloadData()
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("successfully connected to \(peripheral.name)")
+    }
+    
+}
+
+extension MainViewController : UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peripheral.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "bluetoothDeviceCell") as! BluetoothDeviceCell
+        let tempArray = Array(peripheral)
+        cell.nameLabel.text = tempArray[indexPath.row].value.name ?? "N/A"
+        cell.uuidLabel.text = tempArray[indexPath.row].value.identifier.uuidString
+        return cell
+    }
+    
+}
+
+extension MainViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let tempArray = Array(peripheral)
+        selectedPeripheral = tempArray[indexPath.row].value
+        //selectedPeripheral.delegate = self
+        centralManager.stopScan()
+        centralManager.connect(selectedPeripheral!)
     }
 }
